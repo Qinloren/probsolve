050ad96618fb70cf0980094dda76f24fa6e7ceae
Qinloren <2336249419@qq.com>
2025-11-14 14:53:33 +0800

    全局异常响应内容处理

--- src/main/java/com/zeeyeh/probsolve/controller/response/GlobalExceptionHandler.java
+++ src/main/java/com/zeeyeh/probsolve/controller/response/GlobalExceptionHandler.java
@@ -10,7 +10,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
