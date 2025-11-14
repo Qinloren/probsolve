71ddeaf10c63046a3166f6823a5c93f5115dc7c8
Qinloren <2336249419@qq.com>
2025-11-14 13:48:49 +0800

    项目初始化

---
+++ src/main/java/com/zeeyeh/probsolve/ProbsolveApplication.java
@@ -0,0 +1,13 @@
+package com.zeeyeh.probsolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbsolveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProbsolveApplication.class, args);
+    }
+
+}
